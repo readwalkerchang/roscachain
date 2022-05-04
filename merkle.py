496256244aed7317830062a741e61d7452f409ba
Dusing Chang <dusing.chang@mendix.com>
2022-05-04 14:12:40 +0200

    pass first test of merkle tree

--- merkle.py
+++ merkle.py
@@ -1,23 +1,30 @@
+import hashlib
+
 listToHash = ['hello','world','hello','world']
 branchList = []
+
+def hash(val: str)-> str:
+    return hashlib.sha256(val.encode("utf-8")).hexdigest()
 
 def CreateBranch(left,right):
     return hash(hash(left)+hash(right))
 
 def MakeBranch(inputList:list):
-    while(len(listToHash) > 0):
+    branchList = []
+    while(len(inputList) > 0):
         left = inputList.pop(0)
         right = inputList.pop(0)
         branchList.append(CreateBranch(left,right))
+    return branchList
 
         
 def BuildTree(inputList:list):
     branchList = []
     inputList = inputList
     while(len(branchList) != 1):
-        MakeBranch(inputList)
-        # if len(listToHash) == 0 &  len(branchList) != 1:
-        #     branchList = []
-        #     inputList = branchList
+        branchList = MakeBranch(inputList)
+        if len(inputList) == 0 &  len(branchList) != 1:
+            inputList = branchList
+    return branchList
 
 print(BuildTree(listToHash))
