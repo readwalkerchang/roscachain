827387d5f3ff2ae866186972e8367c3a8bdc420d
Dusing Chang <dusing.chang@mendix.com>
2022-05-11 19:20:55 +0200

    run a ROSCＡ　ｗｉｔｈ　５　ｍｅｅｔｉｎｇｓ　ｗｉｔｈ　５　ｕｓｅｒｓ

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from userAccount import User
+from userAccount import Pool
 import json
 import os
 from datetime import datetime
@@ -38,10 +39,22 @@ def createUserDB(userName:str):
         f.write(jsonStr)
     return newUser
 
+def createPoolDB():
+    #1. create a file with user's name
+    newPool = Pool().__dict__
+    jsonStr = json.dumps(newPool)
+    save_path = 'data'
+    file_name = json.loads(jsonStr)['name'] + '.json'
+    completeName = os.path.join(save_path, file_name)
+    #2. put the account message into the file
+    with open(completeName, "w") as f :
+        f.write(jsonStr)
+    return newPool
+
 def sendCurrency(sender:object,receiver:object,amount:int):
-     senderBalance = sender['balance'] 
+     senderBalance = int(sender['balance']) 
      sender['balance'] = senderBalance - amount
-     receiverBalance = receiver['balance'] 
+     receiverBalance = int(receiver['balance'] )
      receiver['balance'] = receiverBalance + amount
      writeJson(sender)
      writeJson(receiver)
@@ -54,30 +67,38 @@ user2 = createUserDB('Amy')
 user3 = createUserDB('Lisa')
 user4 = createUserDB('Jason')
 user5 = createUserDB('Zoey')
-pool = createUserDB('Pool')
+pool = createPoolDB()
 
 
-listToHash = []
+# listToHash = []
+# listToHash.append(sendCurrency(user1,pool,10))
+# listToHash.append(sendCurrency(user2,pool,10))
+# listToHash.append(sendCurrency(user3,pool,10))
+# listToHash.append(sendCurrency(user4,pool,10))
+# listToHash.append(sendCurrency(user5,pool,10))
 
-
-s = Blockchain()
-s.addBlock(listToHash)
+# s = Blockchain()
+# s.addBlock(listToHash)
 # s.addBlock(['one','two'])
 
-blockdict = s.head.__dict__
+# blockdict = s.head.__dict__
 
-jsonStr = json.dumps(blockdict)
-writeJsonBlock(jsonStr)
+# jsonStr = json.dumps(blockdict)
+# writeJsonBlock(jsonStr)
 
 
-#Run ROSCA
-# for x in range(10):
-#     listToHash.append(sendCurrency(user1,pool,10))
-#     listToHash.append(sendCurrency(user2,pool,10))
-#     listToHash.append(sendCurrency(user3,pool,10))
-#     listToHash.append(sendCurrency(user4,pool,10))
-#     listToHash.append(sendCurrency(user5,pool,10))
-#     print('the ' +str(x)+' meeting')
+###Run ROSCA
 
+userlist = [user1, user2, user3, user4, user5]
+
+def runROSCA(meetingTimes:int, amount:int, userList:list,pool:dict):
+    for x in range(meetingTimes):
+        print('the ' +str(x+1)+' meeting')
+        for y in userList:
+            sendCurrency(y,pool,amount)
+
+        sendCurrency(pool,userList[x],pool['balance'])
+
+runROSCA(5,10,userlist,pool)
 
 
--- userAccount.py
+++ userAccount.py
@@ -3,5 +3,11 @@ class User:
         self.name = name
         self.address = ''
         self.trustValue = 0
-        self.balance = 100
+        self.balance = 50
         
+        
+class Pool:
+    def __init__(self):
+        self.name = 'pool'
+        self.address = ''
+        self.balance = 0
