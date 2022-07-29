99d1277eba397fa025e8b85e93ccb1bef8b15e07
Dusing Chang <dusing.chang@mendix.com>
2022-07-29 04:39:48 +0200

    demo02 prepare

--- demo02.py
+++ demo02.py
@@ -6,24 +6,18 @@ from  blockchain import Blockchain
 import numpy as np
 data1 = np.loadtxt("data.csv")
 
+
+
 user1 = createUserDB('Bob',data1[101])
 user2 = createUserDB('Amy',data1[102])
 user3 = createUserDB('Lisa',data1[103])
 user4 = createUserDB('Jason',data1[104])
 user5 = createUserDB('Zoey',data1[105])
 pool = createPoolDB()
+OriginalTrust = "Original trust values:\n{0}\n{1}\n{2}\n{3}\n{4}\n".format(data1[101], data1[102],data1[103],data1[104],data1[105])
 
 
-listToHash1 = []
-listToHash1.append(sendCurrency(user1,pool,1000))
-listToHash1.append(sendCurrency(user2,pool,1000))
-listToHash1.append(sendCurrency(user3,pool,1000))
-listToHash1.append(sendCurrency(user4,pool,1000))
-listToHash1.append(sendCurrency(user5,pool,1000))
+userlist = [user1, user2, user3, user4, user5]  
+runROSCA(5,1000,userlist)
 
-
-s = Blockchain()
-s.addBlock(listToHash1)
-blockdict = s.head.__dict__
-writeJsonBlock(blockdict)
-print(blockdict)
+print(OriginalTrust)
